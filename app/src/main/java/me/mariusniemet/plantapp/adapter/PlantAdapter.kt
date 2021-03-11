3932d7ec07a4dbd25f6b422e3eceb8629ab3099d
niemet0502 <vincentmarius8@gmail.com>
2021-03-11 11:42:09 +0100

    main :: PlantAdapter

--- app/src/main/java/me/mariusniemet/plantapp/adapter/PlantAdapter.kt
+++ app/src/main/java/me/mariusniemet/plantapp/adapter/PlantAdapter.kt
@@ -1,4 +1,27 @@
 package me.mariusniemet.plantapp.adapter
 
-class PlantAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import me.mariusniemet.plantapp.R
+
+class PlantAdapter :RecyclerView.Adapter<PlantAdapter.ViewHolder>() {
+    // boite pour ranger tous les composants a controler
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        // image
+        val plantImage = view.findViewById<ImageView>(R.id.image_item)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_horizontal_plant, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = 5
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
