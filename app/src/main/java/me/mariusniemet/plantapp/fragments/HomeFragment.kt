edad36681e4f3721977af9c7e07f00b79b47f40c
niemet0502 <vincentmarius8@gmail.com>
2021-03-11 10:37:18 +0100

    main :: created HomeFragment

--- app/src/main/java/me/mariusniemet/plantapp/fragments/HomeFragment.kt
+++ app/src/main/java/me/mariusniemet/plantapp/fragments/HomeFragment.kt
@@ -1,4 +1,14 @@
 package me.mariusniemet.plantapp.fragments
 
-class HomeFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import me.mariusniemet.plantapp.R
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.fragment_home, container, false)
+    }
 }
