2e3fb111cd55b089483b972b856828c8b9c03a14
niemet0502 <vincentmarius8@gmail.com>
2021-03-11 11:41:48 +0100

    main :: update MainActivity

--- app/src/main/java/me/mariusniemet/plantapp/MainActivity.kt
+++ app/src/main/java/me/mariusniemet/plantapp/MainActivity.kt
@@ -2,10 +2,18 @@ package me.mariusniemet.plantapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import me.mariusniemet.plantapp.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // integrer le fragment dans notre container
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, HomeFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
